c7faaf367bd529767181a3759d761fe90b0d63f5
Felix <jazzfelix@posteo.de>
2022-12-14 14:41:26 +0100

    added stub core library, removed unnecessary variables

---
+++ inc/core.h
@@ -0,0 +1,2 @@
+#define ALWAYS_INLINE status __attribute__((always_inline)) inline
+
--- inc/midi.h
+++ inc/midi.h
@@ -6,17 +6,16 @@
 #define MIDI_SEND_BUFFER_SIZE 8
 
 struct midi_s {
-    uint8_t channel;
     uint8_t tmp;
+    uint8_t channel;
+    uint8_t rx_byte;
+    uint8_t cmd;
     uint8_t stage;
-    int note_wh;
     uint8_t note_tmp;
     uint8_t mask;
     uint8_t size;
     uint8_t offset;
     uint8_t var;
-    uint8_t kmd_short;
-    uint8_t kmd_long;
     uint8_t data[2];
     uint8_t velocity;
     uint8_t tick;
@@ -24,9 +23,6 @@ struct midi_s {
     uint8_t send_n;
     uint8_t send_i;
     uint8_t sent_cc[128];
-    int poly_notes[4];
-    int assigned_voice[4];
-    unsigned char poly_gate_bits;
 };
 
 #endif	/* MIDI_H */
