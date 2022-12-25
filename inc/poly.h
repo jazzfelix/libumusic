edb782a128122257b078f8f2b6fb440b773b4ab9
Felix <jazzfelix@posteo.de>
2022-12-25 08:43:34 +0100

    rework of the polyphony algorithm in pseudo code

--- inc/poly.h
+++ inc/poly.h
@@ -6,87 +6,57 @@
 
 struct lum_poly_s
 {
-	int8_t note_container[LUM_POLY_VOICES];
-	int8_t trigger_container[LUM_POLY_VOICES];
-	int8_t velocity_container[LUM_POLY_VOICES];
-	int8_t associated_voice[LUM_POLY_VOICES];
-	int8_t stolen_notes[128];
-	int8_t stolen_velocities[128];
-	int8_t num_stolen_notes;
-};
-
-struct lum_note_s
-{
-	uint8_t number;
-	uint8_t velocity; /* velocity = 0 => note off */
 };
 
 struct lum_poly_s lum_init_poly (void)
 {
 	struct lum_poly_s lum_poly;
-	uint8_t i;
-	for (i = 0; i < LUM_POLY_VOICES; i++)
-	{
-		lum_poly.note_container[i] = -1;
-		lum_poly.trigger_container[i] = -1;
-		lum_poly.velocity_container[i] = 0;
-		lum_poly.associated_voice[i] = -1;
-	}
-	for (i = 0; i < 128; i++)
-	{
-		lum_poly.stolen_notes[i] = -1;
-		lum_poly.stolen_velocities[i] = -1;
-	}
-	lum_poly.num_stolen_notes = 0;
 	return lum_poly;
 }
 
 void lum_poly (struct lum_poly_s *poly, struct lum_note_s note)
 {
 #ifdef BOTTOM2ND
-	/* algorithm:
-	 * this is tedious but efficient
-	 * the inefficient way is much easier to implement
-	 * steal the note second from bottom
-	 * count leading zeros might be a shortcut
+	/* Algorithm:
+	 * Always steal the 2nd note from bottom if there are more keys pressed then voices available.
+	 * In other words:
+	 * Play the bottom note, play (number of voices - 1) notes from top.
 	 *
-	 * note on:
-	 * 1. compare from bottom to top:
-	 *    while new note is higher, mark position for insertion (stops when note is not higher)
-	 * 2. if marked position is not empty:
-	 *    move old note from marked position to stolen_notes and sort stolen notes (highest first)
-	 * 3. move middle notes 1 position down
-	 * 4. insert new note at marked position
-	 * 5. trigger adsr
+	 * This is how we do it:
+	 * Mirror the whole keyboard in an array[128].
+	 * In there store velocity and aftertouch as well as trigger information.
+	 * Have two arrays of a total of 128 bits to store each keypress.
+	 * The first of them is "little endian" the second is "big endian".
+	 * These arrays serve as a shortcut for finding the lowest and topmost notes.
+	 * Thus we can make use of a "count leading zeros" instruction if available on our hardware.
+	 * We also want to know how many keys are pressed at ant moment.
+	 * We need to consider checking if we accidentally received a note on or off twice.
 	 *
-	 * note off:
-	 * 1. check if note is being played
-	 * 2. remove note
-	 * 3. move lower notes up
-	 * 4. find highest stolen note
-	 * 5. insert highest stolen note at 2nd lowest position without retriggering adsr
+	 * Pseudo Code:
+	 * If Note On:
+	 * 	1. Check the note array (do we retrigger?).
+	 * 	2. If the last question was answered with "no" add 1 to the keypress counter.
+	 * 	3. Update the note array.
+	 * 	4. Update the keypress bit arrays.
+	 * Else (Note Off):
+	 * 	1. Check the note array (is the key already off?).
+	 * 	2. If the last question was answered with "no" subtract 1 from the keypress counter.
+	 * 	3. Update the note array.
+	 * 	4. Update the keypress bit arrays.
+	 * 5. Set a variable x = number of keys pressed.
+	 * 	if x > 0:
+	 * 		find bottom note, play it
+	 * 		x = x - 1
+	 * 	while x > 1: // makes sure we have 1 extra note
+	 * 		find next note from top, play it
+	 * 		x = x - 1
+	 * 	play the new note // makes sure we trigger the new note and steal from somewhere top
+	 * 6. Done.
+	 * To play a note means to assign it to a voice.
+	 * I think this description of the algorithm is sufficient to the point it can
+	 * be just implemented. :)
 	 *
 	 */
-	uint8_t i;
-	uint8_t marked_position;
-	struct lum_note_s stolen_note;
-
-	/* 1. compare from bottom to top */
-	marked_position = 0;
-	while (poly->note_container[marked_position] < note.number)
-	{
-		marked_position++;
-		if (marked_position > LUM_POLY_VOICES)
-		{
-			break;
-		}
-	}
-	/* 2. move old note to stolen notes */
-	if (poly->note_container[marked_position] > -1)
-	{ /* marked position is not empty */
-		/* 2. sort stolen notes */
-		
-	}
 
 #endif
 }
