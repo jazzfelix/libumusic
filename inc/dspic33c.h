9408bd472e606f53d2188b73e45f0818eb5eda77
Felix <jazzfelix@posteo.de>
2023-01-03 15:14:42 +0100

    additions and optimisations in polyphony code

--- inc/dspic33c.h
+++ inc/dspic33c.h
@@ -51,7 +51,12 @@ lum_ff1l (uint16_t x)
 uint8_t static __attribute__((always_inline, flatten)) inline
 lum_ff1r (uint16_t x)
 {
-	return 0;
+	uint16_t result;
+	asm ("FF1R %0, %1"
+		: "=r"(result)
+		: "r"(x)
+	);
+	return result;
 }
 
 /* count leading zeros algorithm for lum_poly polyphony handler */
@@ -59,18 +64,28 @@ uint8_t static __attribute__((always_inline, flatten)) inline
 lum_poly_ff1l (keybits_t *keys)
 {
 	uint16_t i;
-	int16_t count = 0;
+	int16_t count = -1;
 	uint16_t tmp;
+	SRbits.C = 0;
 	for (i = 0; i < (128 / sizeof (keybits_t) / 8); i++)
 	{
 		tmp = lum_ff1l(keys[i]);
+		if (SRbits.C)
+		{
+			count += sizeof (keybits_t) * 8;
+		} else {
+			count += tmp;
+			break;
+		}
+		/*
 		if (tmp > 0)
 		{
-			count += tmp - 1;
+			count += tmp;
 			break;
 		} else {
 			count += sizeof (keybits_t) * 8;
 		}
+		*/
 	}
 	return count;
 }
@@ -78,7 +93,23 @@ lum_poly_ff1l (keybits_t *keys)
 uint8_t static __attribute__((always_inline, flatten)) inline
 lum_poly_ff1r (keybits_t *keys, uint8_t *start)
 {
-	return 0;
+	uint16_t i;
+	int16_t count;
+	uint16_t tmp;
+	count = ((*start >> 3) << 3) + 8;
+	SRbits.C = 0;
+	for (i = *start / sizeof (keybits_t) / 8; i >= 0; i--)
+	{
+		tmp = lum_ff1r(keys[i]);
+		if (SRbits.C)
+		{
+			count -= sizeof (keybits_t) * 8;
+		} else {
+			count -= tmp;
+			break;
+		}
+	}
+	return count;
 }
 
 #endif /* LUM_UC_H */
