3b7e696f0af6780e91279702fa2e3426fe58b313
Felix <jazzfelix@posteo.de>
2023-01-11 16:47:44 +0100

    fixed some bugs in dspic33c polyphony code

--- inc/dspic33c.h
+++ inc/dspic33c.h
@@ -12,7 +12,7 @@ typedef uint16_t keybits_t;
 #define MIDI_RX_BYTE U1RXREG
 
 void static __attribute__((always_inline, flatten)) inline
-lum_poly_fill_array (uint8_t note, keybits_t *keybits[128 / sizeof (keybits_t) / 8])
+lum_poly_fill_array (uint8_t note, keybits_t (keybits[]))
 {
 	int16_t i;
 	i = 0;
@@ -21,11 +21,11 @@ lum_poly_fill_array (uint8_t note, keybits_t *keybits[128 / sizeof (keybits_t) /
 		i++;
 		note -= sizeof (keybits_t) * 8;
 	}
-	keybits[i] = *keybits[i] | (1 << (15 - note));
+	keybits[i] = keybits[i] | (1 << (15 - note));
 }
 
 void static __attribute__((always_inline, flatten)) inline
-lum_poly_clr_array (uint8_t note, keybits_t *keybits[128 / sizeof (keybits_t) / 8])
+lum_poly_clr_array (uint8_t note, keybits_t keybits[])
 {
 	int16_t i;
 	i = 0;
@@ -34,7 +34,7 @@ lum_poly_clr_array (uint8_t note, keybits_t *keybits[128 / sizeof (keybits_t) / 
 		i++;
 		note -= sizeof (keybits_t) * 8;
 	}
-	keybits[i] = *keybits[i] & !(1 << (15 - note));
+	keybits[i] = keybits[i] & !(1 << (15 - note));
 }
 
 uint16_t static __attribute__((always_inline, flatten)) inline
@@ -63,29 +63,19 @@ lum_ff1r (uint16_t x)
 uint8_t static __attribute__((always_inline, flatten)) inline
 lum_poly_ff1l (keybits_t *keys)
 {
-	uint16_t i;
+	int16_t i;
 	int16_t count = -1;
 	uint16_t tmp;
-	SRbits.C = 0;
 	for (i = 0; i < (128 / sizeof (keybits_t) / 8); i++)
 	{
 		tmp = lum_ff1l(keys[i]);
-		if (SRbits.C)
+		if (tmp == 0)
 		{
 			count += sizeof (keybits_t) * 8;
 		} else {
 			count += tmp;
 			break;
 		}
-		/*
-		if (tmp > 0)
-		{
-			count += tmp;
-			break;
-		} else {
-			count += sizeof (keybits_t) * 8;
-		}
-		*/
 	}
 	return count;
 }
@@ -93,15 +83,14 @@ lum_poly_ff1l (keybits_t *keys)
 uint8_t static __attribute__((always_inline, flatten)) inline
 lum_poly_ff1r (keybits_t *keys, uint8_t *start)
 {
-	uint16_t i;
+	int16_t i;
 	int16_t count;
 	uint16_t tmp;
 	count = ((*start >> 3) << 3) + 8;
-	SRbits.C = 0;
 	for (i = *start / sizeof (keybits_t) / 8; i >= 0; i--)
 	{
 		tmp = lum_ff1r(keys[i]);
-		if (SRbits.C)
+		if (tmp == 0)
 		{
 			count -= sizeof (keybits_t) * 8;
 		} else {
